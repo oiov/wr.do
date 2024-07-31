e6b618741a8f3e5252a5fa1e6c5f5e12995d582f
oiov <stacystayfine718@gmail.com>
2024-07-31 20:23:28 +0800

    fix: user role sider

--- components/layout/dashboard-sidebar.tsx
+++ components/layout/dashboard-sidebar.tsx
@@ -107,52 +107,31 @@ export function DashboardSidebar({ links }: DashboardSidebarProps) {
               </div>
 
               <nav className="flex flex-1 flex-col gap-8 px-4 pt-4">
-                {links.map((section) => (
-                  <section
-                    key={section.title}
-                    className="flex flex-col gap-0.5"
-                  >
-                    {isSidebarExpanded ? (
-                      <p className="text-xs text-muted-foreground">
-                        {section.title}
-                      </p>
-                    ) : (
-                      <div className="h-4" />
-                    )}
-                    {section.items.map((item) => {
-                      const Icon = Icons[item.icon || "arrowRight"];
-                      return (
-                        item.href && (
-                          <Fragment key={`link-fragment-${item.title}`}>
-                            {isSidebarExpanded ? (
-                              <Link
-                                key={`link-${item.title}`}
-                                href={item.disabled ? "#" : item.href}
-                                className={cn(
-                                  "flex items-center gap-3 rounded-md p-2 text-sm font-medium hover:bg-muted",
-                                  path === item.href
-                                    ? "bg-muted"
-                                    : "text-muted-foreground hover:text-accent-foreground",
-                                  item.disabled &&
-                                    "cursor-not-allowed opacity-80 hover:bg-transparent hover:text-muted-foreground",
-                                )}
-                              >
-                                <Icon className="size-5" />
-                                {item.title}
-                                {item.badge && (
-                                  <Badge className="ml-auto flex size-5 shrink-0 items-center justify-center rounded-full">
-                                    {item.badge}
-                                  </Badge>
-                                )}
-                              </Link>
-                            ) : (
-                              <Tooltip key={`tooltip-${item.title}`}>
-                                <TooltipTrigger asChild>
+                {links.map(
+                  (section) =>
+                    section.items.length > 0 && (
+                      <section
+                        key={section.title}
+                        className="flex flex-col gap-0.5"
+                      >
+                        {isSidebarExpanded ? (
+                          <p className="text-xs text-muted-foreground">
+                            {section.title}
+                          </p>
+                        ) : (
+                          <div className="h-4" />
+                        )}
+                        {section.items.map((item) => {
+                          const Icon = Icons[item.icon || "arrowRight"];
+                          return (
+                            item.href && (
+                              <Fragment key={`link-fragment-${item.title}`}>
+                                {isSidebarExpanded ? (
                                   <Link
-                                    key={`link-tooltip-${item.title}`}
+                                    key={`link-${item.title}`}
                                     href={item.disabled ? "#" : item.href}
                                     className={cn(
-                                      "flex items-center gap-3 rounded-md py-2 text-sm font-medium hover:bg-muted",
+                                      "flex items-center gap-3 rounded-md p-2 text-sm font-medium hover:bg-muted",
                                       path === item.href
                                         ? "bg-muted"
                                         : "text-muted-foreground hover:text-accent-foreground",
@@ -160,22 +139,46 @@ export function DashboardSidebar({ links }: DashboardSidebarProps) {
                                         "cursor-not-allowed opacity-80 hover:bg-transparent hover:text-muted-foreground",
                                     )}
                                   >
-                                    <span className="flex size-full items-center justify-center">
-                                      <Icon className="size-5" />
-                                    </span>
+                                    <Icon className="size-5" />
+                                    {item.title}
+                                    {item.badge && (
+                                      <Badge className="ml-auto flex size-5 shrink-0 items-center justify-center rounded-full">
+                                        {item.badge}
+                                      </Badge>
+                                    )}
                                   </Link>
-                                </TooltipTrigger>
-                                <TooltipContent side="right">
-                                  {item.title}
-                                </TooltipContent>
-                              </Tooltip>
-                            )}
-                          </Fragment>
-                        )
-                      );
-                    })}
-                  </section>
-                ))}
+                                ) : (
+                                  <Tooltip key={`tooltip-${item.title}`}>
+                                    <TooltipTrigger asChild>
+                                      <Link
+                                        key={`link-tooltip-${item.title}`}
+                                        href={item.disabled ? "#" : item.href}
+                                        className={cn(
+                                          "flex items-center gap-3 rounded-md py-2 text-sm font-medium hover:bg-muted",
+                                          path === item.href
+                                            ? "bg-muted"
+                                            : "text-muted-foreground hover:text-accent-foreground",
+                                          item.disabled &&
+                                            "cursor-not-allowed opacity-80 hover:bg-transparent hover:text-muted-foreground",
+                                        )}
+                                      >
+                                        <span className="flex size-full items-center justify-center">
+                                          <Icon className="size-5" />
+                                        </span>
+                                      </Link>
+                                    </TooltipTrigger>
+                                    <TooltipContent side="right">
+                                      {item.title}
+                                    </TooltipContent>
+                                  </Tooltip>
+                                )}
+                              </Fragment>
+                            )
+                          );
+                        })}
+                      </section>
+                    ),
+                )}
               </nav>
 
               {isSidebarExpanded && (
