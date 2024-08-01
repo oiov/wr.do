e397767744b20ef64989fa374520d7bdd1a29e9e
oiov <oiov@duck.com>
2024-08-01 20:54:34 +0800

    upd: dash loading style

--- app/(protected)/dashboard/loading.tsx
+++ app/(protected)/dashboard/loading.tsx
@@ -5,8 +5,15 @@ export default function DashboardLoading() {
   return (
     <>
       <DashboardHeader heading="Dashboard" text="" />
-      <Skeleton className="h-[200px] w-full rounded-lg" />
-      <Skeleton className="h-[200px] w-full rounded-lg" />
+      <div className="flex flex-col gap-5">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-3">
+          <Skeleton className="h-32 w-full rounded-lg" />
+          <Skeleton className="h-32 w-full rounded-lg" />
+          <Skeleton className="h-32 w-full rounded-lg" />
+        </div>
+        <Skeleton className="h-[250px] w-full rounded-lg" />
+        <Skeleton className="h-[250px] w-full rounded-lg" />
+      </div>
     </>
   );
 }
