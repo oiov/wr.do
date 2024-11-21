2947f902906a25a5154de1fe5470dee55da8462c
oiov <oiov@duck.com>
2024-11-21 14:53:48 +0800

    upd

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -30,10 +30,7 @@ export default async function DashboardPage() {
 
   return (
     <>
-      <DashboardHeader
-        heading="Dashboard"
-        text="WR.DO Beta Launching Now! 🎉"
-      />
+      <DashboardHeader heading="Dashboard" text="" />
       <div className="flex flex-col gap-5">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 xl:grid-cols-3">
           <HeroCard />
