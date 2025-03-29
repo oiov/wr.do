d4745e12342be6b525c6de9a48db669058d3be2d
oiov <oiov@duck.com>
2025-03-29 18:08:48 +0800

    upd

--- app/api/v1/email-catcher/route.ts
+++ app/api/v1/email-catcher/route.ts
@@ -1,7 +1,7 @@
 export async function POST(req: Request) {
   try {
-    const { email } = await req.json();
-    return Response.json({ email });
+    const data = await req.json();
+    return Response.json({ data, msg: "success" });
   } catch (error) {
     return Response.json({ statusText: "Server error" }, { status: 500 });
   }
