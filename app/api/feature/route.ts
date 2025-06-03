f4c9bad648463c349e80278006912646124a239c
oiov <oiov@duck.com>
2025-06-03 15:58:20 +0800

    test env

--- app/api/feature/route.ts
+++ app/api/feature/route.ts
@@ -1,42 +1,12 @@
-import { env } from "@/env.mjs";
-
-console.log(
-  "[out-env]",
-  env.LinuxDo_CLIENT_ID,
-  env.LinuxDo_CLIENT_SECRET,
-  !!(env.LinuxDo_CLIENT_ID && env.LinuxDo_CLIENT_SECRET),
-);
-console.log(
-  "[out-process.env]",
-  process.env.LinuxDo_CLIENT_ID,
-  process.env.LinuxDo_CLIENT_SECRET,
-  !!(process.env.LinuxDo_CLIENT_ID && process.env.LinuxDo_CLIENT_SECRET),
-);
+// import { env } from "@/env.mjs";
 
 export async function GET(req: Request) {
   try {
-    console.error(
-      "[env]",
-      env.LinuxDo_CLIENT_ID,
-      env.LinuxDo_CLIENT_SECRET,
-      !!(env.LinuxDo_CLIENT_ID && env.LinuxDo_CLIENT_SECRET),
-      "[process.env]",
-      process.env.LinuxDo_CLIENT_ID,
-      process.env.LinuxDo_CLIENT_SECRET,
-      !!(process.env.LinuxDo_CLIENT_ID && process.env.LinuxDo_CLIENT_SECRET),
-    );
-    console.log(
-      "[process.env]",
-      process.env.LinuxDo_CLIENT_ID,
-      process.env.LinuxDo_CLIENT_SECRET,
-      !!(process.env.LinuxDo_CLIENT_ID && process.env.LinuxDo_CLIENT_SECRET),
-    );
-
     return Response.json({
-      google: !!(env.GOOGLE_CLIENT_ID && env.GOOGLE_CLIENT_SECRET),
-      github: !!(process.env.GITHUB_ID && process.env.GITHUB_SECRET),
-      linuxdo: !!(env.LinuxDo_CLIENT_ID && env.LinuxDo_CLIENT_SECRET),
-      resend: !!(env.RESEND_API_KEY && env.RESEND_FROM_EMAIL),
+      google: true,
+      github: true,
+      linuxdo: true,
+      resend: true,
     });
   } catch (error) {
     console.log("[Error]", error);
