6cade53ec5f713c5144dfcc461d8f119d9ddae53
oiov <oiov@duck.com>
2025-06-03 15:31:20 +0800

    test env

--- app/api/feature/route.ts
+++ app/api/feature/route.ts
@@ -15,14 +15,12 @@ export async function GET() {
       !!(process.env.LinuxDo_CLIENT_ID && process.env.LinuxDo_CLIENT_SECRET),
     );
 
-    return Response.json(
-      JSON.stringify({
-        google: !!(env.GOOGLE_CLIENT_ID && env.GOOGLE_CLIENT_SECRET),
-        github: !!(process.env.GITHUB_ID && process.env.GITHUB_SECRET),
-        linuxdo: !!(env.LinuxDo_CLIENT_ID && env.LinuxDo_CLIENT_SECRET),
-        resend: !!(env.RESEND_API_KEY && env.RESEND_FROM_EMAIL),
-      }),
-    );
+    return Response.json({
+      google: !!(env.GOOGLE_CLIENT_ID && env.GOOGLE_CLIENT_SECRET),
+      github: !!(process.env.GITHUB_ID && process.env.GITHUB_SECRET),
+      linuxdo: !!(env.LinuxDo_CLIENT_ID && env.LinuxDo_CLIENT_SECRET),
+      resend: !!(env.RESEND_API_KEY && env.RESEND_FROM_EMAIL),
+    });
   } catch (error) {
     console.log("[Error]", error);
   }
