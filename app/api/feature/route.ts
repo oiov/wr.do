22f1686ff74c5e9944b4a3b21fb90900c8930a8d
oiov <oiov@duck.com>
2025-06-03 15:17:54 +0800

    test env

--- app/api/feature/route.ts
+++ app/api/feature/route.ts
@@ -1,5 +1,18 @@
 import { env } from "@/env.mjs";
 
+console.log(
+  "[out-env]",
+  env.LinuxDo_CLIENT_ID,
+  env.LinuxDo_CLIENT_SECRET,
+  !!(env.LinuxDo_CLIENT_ID && env.LinuxDo_CLIENT_SECRET),
+);
+console.log(
+  "[out-process.env]",
+  process.env.LinuxDo_CLIENT_ID,
+  process.env.LinuxDo_CLIENT_SECRET,
+  !!(process.env.LinuxDo_CLIENT_ID && process.env.LinuxDo_CLIENT_SECRET),
+);
+
 export async function GET() {
   try {
     console.log(
@@ -18,7 +31,7 @@ export async function GET() {
     return new Response(
       JSON.stringify({
         google: !!(env.GOOGLE_CLIENT_ID && env.GOOGLE_CLIENT_SECRET),
-        github: !!(env.GITHUB_ID && env.GITHUB_SECRET),
+        github: !!(process.env.GITHUB_ID && process.env.GITHUB_SECRET),
         linuxdo: !!(env.LinuxDo_CLIENT_ID && env.LinuxDo_CLIENT_SECRET),
         resend: !!(env.RESEND_API_KEY && env.RESEND_FROM_EMAIL),
       }),
