5ffa4bda8f01277ef3b7926cb3489715d1e501dd
oiov <oiov@duck.com>
2025-06-08 15:39:57 +0800

    fixup intl fetch error

--- app/(protected)/admin/loading.tsx
+++ app/(protected)/admin/loading.tsx
@@ -4,10 +4,7 @@ import { DashboardHeader } from "@/components/dashboard/header";
 export default function AdminPanelLoading() {
   return (
     <>
-      <DashboardHeader
-        heading="Admin Panel"
-        text="Access only for users with ADMIN role"
-      />
+      <DashboardHeader heading="Admin Panel" text="" />
       <div className="flex flex-col gap-5">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-3">
           <Skeleton className="h-32 w-full rounded-lg" />
--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -142,7 +142,6 @@ export default async function DashboardPage() {
 
   return (
     <>
-      {/* <DashboardHeader heading="Dashboard" text="" /> */}
       <div className="flex flex-col gap-5">
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 xl:grid-cols-3">
           <ErrorBoundary
