bc20bb2fec5f91381896f20e29ef45eb9ad2048a
oiov <oiov@duck.com>
2025-08-05 20:17:28 +0800

    fix file list user info display

--- components/file/file-list.tsx
+++ components/file/file-list.tsx
@@ -357,13 +357,13 @@ export default function UserFileList({
                 <ClickableTooltip
                   className="cursor-pointer truncate"
                   content={
-                    <div className="p-2">
+                    <div className="px-2 py-1">
                       <p>{file.user.name}</p>
                       <p>{file.user.email}</p>
                     </div>
                   }
                 >
-                  {file.user.name ?? file.user.email}
+                  {file.user.name || file.user.email}
                 </ClickableTooltip>
               </div>
               <div className="col-span-1 hidden items-center text-nowrap text-xs sm:flex">
